07660b3d722b1d4947da54f9dba95376931d7f37
Aatish chaudhari <40951689+Aatish13@users.noreply.github.com>
2019-03-10 20:56:43 +0530

    reports added

--- CRM_software/manager/views.py
+++ CRM_software/manager/views.py
@@ -12,13 +12,20 @@ from employee.models import employee_customer
 @login_required(login_url = '/accounts/login/')
 def dashboard(request):
     if request.session['user_type']=='manager':
+        salesData=[0]*13
+        salesCount = [0] * 13
         sales = employee_customer.objects.all()
         for sal in sales:
+            salesData[sal.r_date.month]=int(salesData[sal.r_date.month])+int(sal.product.price)
             print(sal.c_name,sal.product.price,sal.r_date.month,sal.r_date.year)
-        #t = employee_customer.objects.raw(
-         #   "SELECT COUNT(id) as c,id,r_date FROM `employee_employee_customer` GROUP by month(r_date),e_id ")
+        print(sales[len(sales)-1].r_date.month)
+        salcount = employee_customer.objects.raw("SELECT count(id) as count,id,r_date,product_id FROM `employee_employee_customer` GROUP by month(r_date),year(r_date) ")
+        for x in salcount:
+            print(x.id,x.count,x.r_date,x.product.price)
+            salesCount[x.r_date.month]=x.count
         data = [299, 3000, 2000, 1000, 1111, 111,2222, 5499,2222]
-        return render(request,'mandashboard.html',{"data":data,"sales":sales})
+
+        return render(request,'mandashboard.html',{"data":salesCount[1:sales[len(sales)-1].r_date.month+1],"sales":sales,"salesData":salesData[1:sales[len(sales)-1].r_date.month+1]})
     else:
         message="Login in as manager to access this page."
         return render(request,'error.html',{'message':message})
